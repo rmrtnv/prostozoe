26378e11b233579c4d9694d254e96fcbb2800ace
rmrtnv <yus.online@gmail.com>
2024-12-11 15:37:49 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Просто Зое</title>
+  <!-- b0a8e2d8ccb04b24683d347076e80d29e451a385:d3aa2e6571e673001cb012eda23bd97d02234f0b -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <style>
+  @font-face {
+    font-family: 'Fetish'; /*a name to be used later*/
+    src: url('/fashionfetishregular.ttf'); /*URL to font*/
+}
+    html {
+      height: 100%;
+      background: #B7605E;
+      font-family: 'Fetish';
+    }
+
+  </style>
+</head>
+<body>
+<section class="logo" style="
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+">
+        <img src="/img/logo/zoe.svg" style="
+    max-width: 100%;
+    max-height: 600px;
+">
+    </section>
+
+<section class="text" style="
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+">
+<div style="
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1em;
+    font-size: 1em;
+    margin-bottom: 2em;
+">
+        <span>Пятигорск, проспект Кирова, 94</span>
+        <span>+7 961 458-88-85</span>
+        <span>zoebistro@yandex.ru</span>
+        <span>social media prosto.zoe</span>   
+    </div>
+
+    </section>
+</body>
+</html>
